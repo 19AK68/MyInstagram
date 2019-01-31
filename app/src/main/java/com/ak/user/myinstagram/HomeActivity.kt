c6ccb06f47c1b5d54785e73190a12d47b20cc4d0
Andrey Kushnaryov <akushnaryov@rightandabove.com>
2019-01-31 15:27:30 +0200

    auth firebase

--- app/src/main/java/com/ak/user/myinstagram/HomeActivity.kt
+++ app/src/main/java/com/ak/user/myinstagram/HomeActivity.kt
@@ -2,7 +2,7 @@ package com.ak.user.myinstagram
 
 import android.os.Bundle
 import android.util.Log
-
+import com.google.firebase.auth.FirebaseAuth
 
 
 class HomeActivity : BaseActivity(0) {
@@ -15,6 +15,19 @@ class HomeActivity : BaseActivity(0) {
         setContentView(R.layout.activity_home)
         setupButtonNavigation()
         Log.d(TAG, "onCreate")
+
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword("ahdpei68@gmail.com","280367")
+            .addOnCompleteListener{
+                if(it.isSuccessful){
+                    Log.d(TAG,"singIn success")
+                } else {
+                    Log.e(TAG,"singIn: failure",it.exception)
+                }
+            }
+
+
+
 
     }
 }
